f1d63d802718724da0fd793a3646a7a4fb9dabb3
LuisHoyosT <luishoyos228@gmail.com>
2024-03-30 16:48:42 -0500

    mas arreglos para la pag Projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './Home.css';
+import React, { useState } from "react";
+import "./Home.css";
 import {
   IonContent,
   IonHeader,
@@ -13,33 +13,54 @@ import {
   IonTitle,
   IonList,
   IonItem,
-  IonLabel
-} from '@ionic/react';
-import { apps } from 'ionicons/icons'; // Importa el icono de Ionic
-import 'react-datepicker/dist/react-datepicker.css'; // Estilos de react-datepicker
-import Calendar from 'react-calendar';
-
+  IonLabel,
+  IonImg,
+} from "@ionic/react";
+import { apps } from "ionicons/icons"; // Importa el icono de Ionic
+import "react-datepicker/dist/react-datepicker.css"; // Estilos de react-datepicker
+import "react-calendar/dist/Calendar.css"; // Estilos de react-calendar
+import Calendar from "react-calendar"; // Importa react-calendar
+import logo from "../img/Logo.png";
 const Home: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [startDate, setStartDate] = useState(new Date()); // Estado para la fecha del calendario
+  const [selectedDate, setSelectedDate] = useState<Date | Date[] | null>(
+    new Date()
+  );
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const handleDateChange = (value: Date | Date[] | null) => {
+    setSelectedDate(value);
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>WorkPlanner</IonTitle>
-          <IonTitle slot="start" className="header-title">Projects</IonTitle>
+            {/* Contenedor del logo y "WorkPlanner" */}
+          <div className="logo-title-container">
+            {/* Logo */}
+            <IonImg src={logo} className="logo" />
+
+            {/* Título "WorkPlanner" */}
+            <IonTitle className="header-title">WorkPlanner</IonTitle>
+          </div>
+
+          {/* Contenedor del título "Projects" */}
+          <div className="projects-title-container">
+            {/* Título "Projects" */}
+            <IonTitle className="projects-title">Projects</IonTitle>
+          </div>
           <IonButton slot="end" onClick={toggleMenu}>
-            <IonIcon name={menuOpen ? 'close-circle' : 'menu'} />
+            <IonIcon name={menuOpen ? "close-circle" : "menu"} />
           </IonButton>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <div className={`menu ${menuOpen ? 'open' : ''}`}>
+        <div className={`menu ${menuOpen ? "open" : ""}`}>
           <IonList className="menu-list">
             <IonItem className="menu-item">
               <IonLabel>In Progress</IonLabel>
@@ -55,35 +76,42 @@ const Home: React.FC = () => {
             </IonItem>
           </IonList>
         </div>
-        {/* Contenido principal aquí */}
+
+        {/*Tarjetas de actividades */}
         <IonCard className="card">
           <IonCardHeader>
             <IonTitle>Card 1</IonTitle>
           </IonCardHeader>
-          <IonCardContent>
-            Content of Card 1
-          </IonCardContent>
+          <IonCardContent>Content of Card 1</IonCardContent>
         </IonCard>
         <IonCard className="card">
           <IonCardHeader>
             <IonTitle>Card 2</IonTitle>
           </IonCardHeader>
-          <IonCardContent>
-            Content of Card 2
-          </IonCardContent>
+          <IonCardContent>Content of Card 2</IonCardContent>
         </IonCard>
         <IonCard className="card">
           <IonCardHeader>
             <IonTitle>Card 3</IonTitle>
           </IonCardHeader>
-          <IonCardContent>
-            Content of Card 3
-          </IonCardContent>
+          <IonCardContent>Content of Card 3</IonCardContent>
         </IonCard>
-        <Calendar className="calendar" />
-        <IonButton expand="block" className="bottom-button">
-          Bottom Button
-        </IonButton>
+
+        {/* Calendario */}
+        <div className="calendar-container">
+          <Calendar
+            onChange={handleDateChange}
+            value={selectedDate as Date | Date[] | null}
+          />
+        </div>
+
+        <div className="centered-button-container">
+          <div className="centered-button">
+            <IonButton shape="round" className="bottom-button">
+              +
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
