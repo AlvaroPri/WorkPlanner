12b40e879514e24c9a3f24f9cdcfb0a6f2f092cd
AlvaroPri <alvaro.mosquera04@usc.edu.co>
2024-04-15 19:17:40 -0500

    Coreccion en router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Redirect, Route } from 'react-router-dom';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './pages/Home';
-import ExploreContainer from './components/ExploreContainer';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -19,15 +16,19 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-/* Theme variables */
+
+/* Componentes y páginas */
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './pages/Home';
+import ExploreContainer from './components/ExploreContainer';
 import './theme/variables.css';
 import InProgress from './pages/InProgress';
 import Complete from './pages/Complete';
 import PendingTask from './pages/PendingTask';
 import U_InProgress from './pages/U_InProgress';
 
-
-
+/* Configuración de Ionic React */
 setupIonicReact();
 
 const App: React.FC = () => (
@@ -40,31 +41,26 @@ const App: React.FC = () => (
         <Route exact path="/register"> 
           <Register />
         </Route>
-        <Route exact path="/explore">
-          <ExploreContainer />
-        </Route>
         <Route exact path="/home">
           <Home />
         </Route>
-
         <Route exact path="/PendingTask">
           <PendingTask />
         </Route>
-
-      
         <Route exact path="/InProgress"> 
           <InProgress />
         </Route>
-
         <Route exact path="/Complete"> 
           <Complete />
         </Route>
-
-        //Rutas para usuarios
+        {/* Rutas para usuario */}
         <Route exact path="/U_InProgress">
           <U_InProgress />
         </Route>
-
+        {/* ExploreContainer debe ser la ruta por defecto */}
+        <Route exact path="/">
+          <ExploreContainer />
+        </Route>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
